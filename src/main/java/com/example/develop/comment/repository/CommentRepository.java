27d99c85a8b26287f68e44cfcb25e876236a4fd2
Nhahan <kisy324@naver.com>
2025-02-13 01:01:28 +0900

    `Lv7`

---
+++ src/main/java/com/example/develop/comment/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.example.develop.comment.repository;
+
+import com.example.develop.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByScheduleId(Long scheduleId);
+}
